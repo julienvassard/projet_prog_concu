39700714c42fc46119c86c198dfcfbecffddbd8c
Wassim Baratli <wassim.baratli@etu.univ-cotedazur.fr~>
2023-12-29 03:53:30 +0100

    Update Concurential between quarts

--- main.py
+++ main.py
@@ -10,20 +10,23 @@ GRID_WIDTH = 512
 GRID_HEIGHT = 128
 
 grid = [['| |' for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
-lock = threading.Lock()
+#lock = threading.Lock()
 
 def process_section(section, x_objectif, y_objectif, personnes, isArrived):
     while not all(isArrived):
         for i, (x, y) in enumerate(personnes):
-            if not isArrived[i]:
-                best_x, best_y = best_path(x, y, x_objectif, y_objectif)
-                if best_x == x_objectif and best_y == y_objectif:
-                    section[y][x] = '| |'
-                    isArrived[i] = True
-                else:
-                    section[y][x] = '| |'
-                    section[best_y][best_x] = f'|{i + 1}|'
-                    personnes[i] = (best_x, best_y)
+            if not isArrived[i] :
+                # and 0 <= x < len(section[0]) and 0 <= y < len(section)
+                #if section[y][x] != '|X|':
+                    best_x, best_y = best_path(x, y, x_objectif, y_objectif)
+                    if best_x == x_objectif and best_y == y_objectif:
+                        section[y][x] = '| |'
+                        isArrived[i] = True
+                    else:
+                        section[y][x] = '| |'
+                        section[best_y][best_x] = f'|{i + 1}|'
+                        personnes[i] = (best_x, best_y)
+
 
 def print_grid(grid):
     for line in grid:
@@ -72,81 +75,122 @@ def best_path(x_personne, y_personne, x_objectif, y_objectif):
     return best_x, best_y
 
 
-def move_person(person_index, x_objectif, y_objectif, isArrived, personnes):
-    while not isArrived[person_index]:
-        x, y = personnes[person_index]
-        with lock:
-            best_x, best_y = best_path(x, y, x_objectif, y_objectif)
-            if best_x == x_objectif and best_y == y_objectif and not isArrived[person_index]:
-                grid[y][x] = '| |'
-                isArrived[person_index] = True
-            elif not isArrived[person_index]:
-                grid[y][x] = '| |'
-                grid[best_y][best_x] = f'|{person_index + 1}|'
-                personnes[person_index] = (best_x, best_y)
+def move_person(person_index,  x_objectif, y_objectif, isArrived, personnes):
+    while person_index < len(isArrived):  # Vérifiez si person_index est dans la plage valide
+        if not isArrived[person_index]:
+            # Assurez-vous que person_index ne dépasse pas la taille de la liste personnes
+            if person_index < len(personnes):
+                x, y = personnes[person_index]
+                best_x, best_y = best_path(x, y, x_objectif, y_objectif)
+                if best_x == x_objectif and best_y == y_objectif:
+                    grid[y][x] = '| |'
+                    isArrived[person_index] = True
+                else:
+                    grid[y][x] = '| |'
+                    grid[best_y][best_x] = f'|{person_index + 1}|'
+                    personnes[person_index] = (best_x, best_y)
+            else:
+                break  # Sortir de la boucle si person_index est invalide
+        else:
+            break  # Sortir de la boucle si la personne est déjà arrivée
+
+
+
 
 
 
 def main(nb_personnes):
-    middle_width = GRID_WIDTH // 2
-    middle_height = GRID_HEIGHT // 2
 
-    section_1 = [row[:middle_width] for row in grid[:middle_height]]
-    section_2 = [row[middle_width:] for row in grid[:middle_height]]
-    section_3 = [row[:middle_width] for row in grid[middle_height:]]
-    section_4 = [row[middle_width:] for row in grid[middle_height:]]
+    # Création de quarts
+    mid_width = GRID_WIDTH // 2
+    mid_height = GRID_HEIGHT // 2
 
-    sections = [section_1, section_2, section_3, section_4]
+    quadrant_1 = [row[:mid_width] for row in grid[:mid_height]]
+    quadrant_2 = [row[mid_width:] for row in grid[:mid_height]]
+    quadrant_3 = [row[:mid_width] for row in grid[mid_height:]]
+    quadrant_4 = [row[mid_width:] for row in grid[mid_height:]]
+
+
+
+    # Initialisation des quadrants avec obstacles et objectif
+    for _ in range(2):
+        x_obstacle = random.randint(0, mid_width - 1)
+        y_obstacle = random.randint(0, mid_height - 1)
+        quadrant_1[y_obstacle][x_obstacle] = '|X|'
+
+        x_obstacle = random.randint(0, mid_width - 1)
+        y_obstacle = random.randint(0, mid_height - 1)
+        quadrant_2[y_obstacle][x_obstacle] = '|X|'
+
+        x_obstacle = random.randint(0, mid_width - 1)
+        y_obstacle = random.randint(0, mid_height - 1)
+        quadrant_3[y_obstacle][x_obstacle] = '|X|'
+
+        x_obstacle = random.randint(0, mid_width - 1)
+        y_obstacle = random.randint(0, mid_height - 1)
+        quadrant_4[y_obstacle][x_obstacle] = '|X|'
+
+    x_objectif = np.random.randint(mid_width, GRID_WIDTH - 1)
+    y_objectif = np.random.randint(mid_height , GRID_HEIGHT - 1)
+
+    quadrant_1[y_objectif - mid_height ][x_objectif - mid_width] = '|T|'
+    quadrant_2[y_objectif - mid_height ][x_objectif - mid_width - mid_width] = '|T|'
+    quadrant_3[y_objectif - mid_height - mid_height ][x_objectif - mid_width] = '|T|'
+    quadrant_4[y_objectif - mid_height - mid_height ][x_objectif - mid_width - mid_width] = '|T|'
+
 
     personnes = []
     isArrived = []
     threads = []
 
-    for i in range(nb_personnes):
-        x_personne = np.random.randint(0, GRID_WIDTH - 1)
-        y_personne = np.random.randint(0, GRID_HEIGHT - 1)
-        personnes.append((x_personne, y_personne))
-        isArrived.append(False)
-        grid[y_personne][x_personne] = f'|{i + 1}|'
+    # Création des threads pour chaque quadrant
+    thread_1 = threading.Thread(target=move_person,
+                                args=(0, x_objectif, y_objectif, isArrived, personnes))
+    thread_2 = threading.Thread(target=move_person,
+                                args=(1, x_objectif, y_objectif, isArrived, personnes))
+    thread_3 = threading.Thread(target=move_person,
+                                args=(2, x_objectif, y_objectif, isArrived, personnes))
+    thread_4 = threading.Thread(target=move_person,
+                                args=(3, x_objectif, y_objectif, isArrived, personnes))
 
-    x_objectif = np.random.randint(0, GRID_WIDTH - 1)
-    y_objectif = np.random.randint(0, GRID_HEIGHT - 1)
-    grid[y_objectif][x_objectif] = '|T|'
+    threads.extend([thread_1, thread_2, thread_3, thread_4])
 
-    threads = []
-    for i in range(nb_personnes):
-        thread = threading.Thread(target=move_person, args=(i, x_objectif, y_objectif, isArrived, personnes))
-        threads.append(thread)
+
+
+    for thread in threads:
         thread.start()
 
     for thread in threads:
         thread.join()
-    #print_grid(grid)
+
 
 
     #print("Nombre personnes:", len(personnes))
     lap = 1
 
-
-
     while True:
         #print("Tour : ", lap)
         for i in range(nb_personnes):
-            x, y = personnes[i]
-            best_x, best_y = best_path(x, y, x_objectif, y_objectif)
-            if best_x == x_objectif and best_y == y_objectif and not isArrived[i]:
-                #print(f"Personne {i + 1} a atteint l'objectif et disparaît !")
-                grid[y][x] = '| |'
-                isArrived[i] = True
-            elif not isArrived[i]:
-                grid[y][x] = '| |'
-                grid[best_y][best_x] = f'|{i + 1}|'
-                personnes[i] = (best_x, best_y)
+            if i < len(personnes):
+                x, y = personnes[i]
+                best_x, best_y = best_path(x, y, x_objectif, y_objectif)
+                if best_x == x_objectif and best_y == y_objectif and not isArrived[i]:
+                    #print(f"Personne {i + 1} a atteint l'objectif et disparaît !")
+                    grid[y][x] = '| |'
+                    isArrived[i] = True
+                elif not isArrived[i]:
+                    grid[y][x] = '| |'
+                    grid[best_y][best_x] = f'|{i + 1}|'
+                    personnes[i] = (best_x, best_y)
+            else:
+                break
 
         if all(isArrived):
             #print("Toutes les personnes ont atteint l'objectif !")
             break
         lap += 1
+
+
 
 
 def measure_execution_time(num_personnes):
@@ -175,3 +219,5 @@ if __name__ == "__main__":
     logging.info("Temps d'exécution (secondes): %s", execution_times)
     logging.info("Speedup: %s", speedup)
     logging.info("Efficacité: %s", efficiency)
+
+
